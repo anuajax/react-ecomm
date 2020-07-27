c8fc9ae8dac54c5f29d5a234d83c921a24918bcb
2017uec1581@mnit.ac.in <2017uec1581@mnit.ac.in>
2020-07-27 19:10:58 +0530

    initial setup

---
+++ src/components/homePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './homePage.styles.scss';
+import MenuItem from './menuComp';
+const Home = () => (
+    <div className="homepage">
+        <MenuItem/>
+    </div>
+)
+export default Home;
---
+++ src/components/menuComp.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import photo1 from  '../images/photo1.jpg'
+const MenuItem = () => (
+    <div className="menu-directory">
+    <div className="menu-item">
+        {/* <img src={photo1}/> */}
+        <div className="content">
+         <h1 className="title">SNEAKERS</h1>
+         <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+    <div className="menu-item">
+        {/* <img src={photo1}/> */}
+        <div className="content">
+         <h1 className="title">SNEAKERS</h1>
+         <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+    <div className="menu-item">
+        {/* <img src={photo1}/> */}
+        <div className="content">
+         <h1 className="title">SNEAKERS</h1>
+         <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+    </div>
+)
+export default MenuItem;
